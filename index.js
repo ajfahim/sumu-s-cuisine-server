5dedb17ec7447f247954b81f4154368aeaafaebe
ajfahim <ajfahim52@gmail.com>
2022-11-30 21:19:53 +0600

    added post user and jwt endpoint

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uh9rxet.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db('sumuscuisine').collection('services');
+        const usersCollection = client.db('sumuscuisine').collection('users')
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = {
+                email: user.email
+            }
+            const checkUser = await usersCollection.findOne(query);
+            if (checkUser) {
+                return res.status(409).send({ message: "user already exits" })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send("server is running")
+})
+
+app.listen(port, () => {
+    console.log(` Server running on ${port}`);
+})
